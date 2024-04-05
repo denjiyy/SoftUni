0ff861eea426d55fff6187fc7caad79ee035a387
Denis <125385668+denjiyy@users.noreply.github.com>
2024-04-05 20:02:56 +0300

    Add files via upload

---
+++ Basic with C#/Cinema/Program.cs
@@ -0,0 +1,22 @@
+﻿string type = Console.ReadLine()!;
+int rows = int.Parse(Console.ReadLine()!);
+int cols = int.Parse(Console.ReadLine()!);
+
+double income = 0.0;
+
+switch (type)
+{
+	case "Premiere":
+		income = rows * cols * 12.00;
+		break;
+	case "Normal":
+		income = cols * rows * 7.50;
+		break;
+	case "Discount":
+		income = rows * cols * 5.00;
+		break;
+	default:
+		break;
+}
+
+Console.WriteLine($"{income:f2} leva");
