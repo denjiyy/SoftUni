e7498582d6c1ebaf65991cb7082a4ae51838cca0
Denis <125385668+denjiyy@users.noreply.github.com>
2024-04-05 20:00:51 +0300

    Add files via upload

---
+++ Basic with C#/TimePlus15Mins/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter hour (0-23): ");
+int hour = int.Parse(Console.ReadLine()!);
+
+Console.Write("Enter minutes (0-59): ");
+int minutes = int.Parse(Console.ReadLine()!);
+
+minutes += 15;
+hour += minutes / 60;
+minutes %= 60;
+hour %= 24;
+
+Console.WriteLine($"It'll be: {hour:D2}:{minutes:D2} after 15 mins.");
