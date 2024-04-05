4be209a2679642ccc708cc1c5d102344d8d079c1
Denis <125385668+denjiyy@users.noreply.github.com>
2024-04-05 20:02:14 +0300

    Add files via upload

---
+++ Basic with C#/SwimmingWorldRecord/Program.cs
@@ -0,0 +1,16 @@
+﻿double recordSeconds = double.Parse(Console.ReadLine()!);
+double distanceMeters = double.Parse(Console.ReadLine()!);
+double timePerMeterSeconds = double.Parse(Console.ReadLine()!);
+
+double resistanceTime = Math.Floor(distanceMeters / 15) * 12.5;
+double swimmingTime = distanceMeters * timePerMeterSeconds + resistanceTime;
+
+if (swimmingTime < recordSeconds)
+{
+    Console.WriteLine($"Yes, he succeeded! The new world record is {swimmingTime:f2} seconds.");
+}
+else
+{
+    double secondsSlower = swimmingTime - recordSeconds;
+    Console.WriteLine($"No, he failed! He was {secondsSlower:f2} seconds slower.");
+}
