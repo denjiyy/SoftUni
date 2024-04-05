f8afcd88a819216774f138a19cc1737185ac30eb
Denis <125385668+denjiyy@users.noreply.github.com>
2024-04-05 20:01:34 +0300

    Add files via upload

---
+++ Basic with C#/GodzillaVsKong/Program.cs
@@ -0,0 +1,26 @@
+﻿double budget = double.Parse(Console.ReadLine()!);
+int extrasCount = int.Parse(Console.ReadLine()!);
+double clothingPricePerExtra = double.Parse(Console.ReadLine()!);
+
+double decorCost = budget * 0.1;
+double clothingCost = extrasCount * clothingPricePerExtra;
+
+double totalCost = decorCost + clothingCost;
+if (extrasCount > 150)
+{
+    clothingCost -= clothingCost * 0.1;
+    totalCost = decorCost + clothingCost;
+}
+
+if (totalCost > budget)
+{
+    double neededMoney = totalCost - budget;
+    Console.WriteLine("Not enough money!");
+    Console.WriteLine($"Wingard needs {neededMoney:f2} leva more.");
+}
+else
+{
+    double remainingMoney = budget - totalCost;
+    Console.WriteLine("Action!");
+    Console.WriteLine($"Wingard starts filming with {remainingMoney:f2} leva left.");
+}
