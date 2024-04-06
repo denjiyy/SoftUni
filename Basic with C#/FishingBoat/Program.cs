944b02f8df128b334523c7a815b436b5324520ac
Denis <125385668+denjiyy@users.noreply.github.com>
2024-04-06 12:49:34 +0300

    Add files via upload

---
+++ Basic with C#/FishingBoat/Program.cs
@@ -0,0 +1,36 @@
+﻿int budget = int.Parse(Console.ReadLine()!);
+string season = Console.ReadLine()!;
+int fishermenCount = int.Parse(Console.ReadLine()!);
+
+double boatRentalPrice = 0;
+
+switch (season)
+{
+    case "Spring":
+        boatRentalPrice = 3000;
+        break;
+    case "Summer":
+    case "Autumn":
+        boatRentalPrice = 4200;
+        break;
+    case "Winter":
+        boatRentalPrice = 2600;
+        break;
+}
+
+if (fishermenCount <= 6)
+    boatRentalPrice *= 0.90;
+else if (fishermenCount <= 11)
+    boatRentalPrice *= 0.85;
+else
+    boatRentalPrice *= 0.75;
+
+if (fishermenCount % 2 == 0 && season != "Autumn")
+    boatRentalPrice *= 0.95;
+
+double totalCost = budget - boatRentalPrice;
+
+if (totalCost >= 0)
+    Console.WriteLine($"Yes! You have {totalCost:f2} leva left.");
+else
+    Console.WriteLine($"Not enough money! You need {Math.Abs(totalCost):f2} leva.");
