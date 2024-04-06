f6bb6b64feff021dcbd8a4c0ecdaeb4654aff83b
Denis <125385668+denjiyy@users.noreply.github.com>
2024-04-06 12:50:28 +0300

    Add files via upload

---
+++ Basic with C#/Roadtrip/Program.cs
@@ -0,0 +1,44 @@
+﻿double budget = double.Parse(Console.ReadLine()!);
+string season = Console.ReadLine()!;
+
+string destination = "";
+string vacationType = "";
+double spentAmount = 0;
+
+if (budget <= 100)
+{
+    destination = "Bulgaria";
+    if (season == "summer")
+    {
+        vacationType = "Camp";
+        spentAmount = budget * 0.30;
+    }
+    else if (season == "winter")
+    {
+        vacationType = "Hotel";
+        spentAmount = budget * 0.70;
+    }
+}
+else if (budget <= 1000)
+{
+    destination = "Balkans";
+    if (season == "summer")
+    {
+        vacationType = "Camp";
+        spentAmount = budget * 0.40;
+    }
+    else if (season == "winter")
+    {
+        vacationType = "Hotel";
+        spentAmount = budget * 0.80;
+    }
+}
+else
+{
+    destination = "Europe";
+    vacationType = "Hotel";
+    spentAmount = budget * 0.90;
+}
+
+Console.WriteLine($"Somewhere in {destination}");
+Console.WriteLine($"{vacationType} - {spentAmount:f2}");
