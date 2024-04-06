293bc8993b7e74892eba318fe23f6b353fb67bfb
Denis <125385668+denjiyy@users.noreply.github.com>
2024-04-06 12:54:49 +0300

    Add files via upload

---
+++ Basic with C#/Volleyball/Program.cs
@@ -0,0 +1,18 @@
+﻿string yearType = Console.ReadLine()!;
+int holidays = int.Parse(Console.ReadLine()!);
+int weekendsBackHome = int.Parse(Console.ReadLine()!);
+
+int weekendsInSofia = 48 - weekendsBackHome;
+
+double volleyballSofiaWeekends = weekendsInSofia * (3.0 / 4);
+double volleyballHolidays = holidays * (2.0 / 3);
+double volleyballBackHome = weekendsBackHome;
+
+double totalVolleyball = volleyballSofiaWeekends + volleyballHolidays + volleyballBackHome;
+
+if (yearType == "leap")
+{
+    totalVolleyball *= 1.15;
+}
+
+Console.WriteLine(Math.Floor(totalVolleyball));
