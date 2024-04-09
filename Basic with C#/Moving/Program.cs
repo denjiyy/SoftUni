b1ecf4e53d88db862ce478d7283f3e3933680c71
Denis <125385668+denjiyy@users.noreply.github.com>
2024-04-09 17:22:20 +0300

    Add files via upload

---
+++ Basic with C#/Moving/Program.cs
@@ -0,0 +1,25 @@
+﻿int freeSpaceWidth = int.Parse(Console.ReadLine()!);
+int freeSpaceLength = int.Parse(Console.ReadLine()!);
+int freeSpaceHeight = int.Parse(Console.ReadLine()!);
+int freeSpace = freeSpaceWidth * freeSpaceLength * freeSpaceHeight;
+bool isFull = false;
+int packetsSum = 0;
+
+string input;
+while ((input = Console.ReadLine()!) != "Done")
+{
+	int packets = int.Parse(input);
+	packetsSum += packets;
+
+	if (packetsSum > freeSpace)
+	{
+        Console.WriteLine($"No more free space! You need {Math.Abs(freeSpace - packetsSum)} Cubic meters more.");
+		isFull = true;
+		break;
+    }
+}
+
+if (isFull == false)
+{
+    Console.WriteLine($"{freeSpace - packetsSum} Cubic meters left.");
+}
