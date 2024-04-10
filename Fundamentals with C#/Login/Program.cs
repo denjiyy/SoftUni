3192a23cc266459b6a92e6dee00091aa5265a508
Denis <125385668+denjiyy@users.noreply.github.com>
2024-04-10 22:06:52 +0300

    Add files via upload

---
+++ Fundamentals with C#/Login/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string username = Console.ReadLine()!;
+        string password = Reverse(username);
+        int attempts = 0;
+
+        while (attempts < 4)
+        {
+            string enteredPassword = Console.ReadLine()!;
+
+            if (enteredPassword == password)
+            {
+                Console.WriteLine($"User {username} logged in.");
+                return;
+            }
+            else
+            {
+                Console.WriteLine("Incorrect password. Try again.");
+                attempts++;
+            }
+        }
+
+        Console.WriteLine($"User {username} blocked!");
+    }
+
+    static string Reverse(string s)
+    {
+        char[] arr = s.ToCharArray();
+        Array.Reverse(arr);
+        return new string(arr);
+    }
+}
