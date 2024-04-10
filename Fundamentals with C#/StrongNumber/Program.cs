cfc6ffa8f521535b7d02e41f53b5780b135c0d54
Denis <125385668+denjiyy@users.noreply.github.com>
2024-04-10 22:19:23 +0300

    Add files via upload

---
+++ Fundamentals with C#/StrongNumber/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static int CalculateFactorial(int number)
+    {
+        int result = 1;
+
+        for (int i = 1; i <= number; i++)
+        {
+            result *= i;
+        }
+
+        return result;
+    }
+
+    static bool IsStrong(int number)
+    {
+        int temp = number, sum = 0;
+
+        while (temp > 0)
+        {
+            int digit = temp % 10;
+            sum += CalculateFactorial(digit);
+            temp /= 10;
+        }
+
+        return sum == number;
+    }
+
+    static void Main()
+    {
+        int number = int.Parse(Console.ReadLine()!);
+        Console.WriteLine(IsStrong(number) ? "yes" : "no");
+    }
+}
