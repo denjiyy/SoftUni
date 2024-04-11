746c3ae8e162b16b52875688ab4570c18c1cc386
Denis <125385668+denjiyy@users.noreply.github.com>
2024-04-11 20:05:00 +0300

    Add files via upload

---
+++ Fundamentals with C#/IntegerOperations/Program.cs
@@ -0,0 +1,9 @@
+﻿int a = int.Parse(Console.ReadLine()!);
+int b = int.Parse(Console.ReadLine()!);
+int c = int.Parse(Console.ReadLine()!);
+int d = int.Parse(Console.ReadLine()!);
+
+int sumPlus = a + b;
+int sumDivision = sumPlus / c;
+int sumMultiply = sumDivision * d;
+Console.WriteLine(sumMultiply);
