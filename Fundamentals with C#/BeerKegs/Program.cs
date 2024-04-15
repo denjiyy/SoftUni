4c5b4e3e17079e92614ba2a04a012f9be45b9fc4
Denis <125385668+denjiyy@users.noreply.github.com>
2024-04-15 16:31:55 +0300

    Add files via upload

---
+++ Fundamentals with C#/BeerKegs/Program.cs
@@ -0,0 +1,19 @@
+﻿int n = int.Parse(Console.ReadLine()!);
+double volume = 0;
+string biggestKegModel = "";
+double biggestKegVolume = 0;
+for (int i = 0; i < n; i++)
+{
+    string model = Console.ReadLine()!;
+    float radius = float.Parse(Console.ReadLine()!);
+    int height = int.Parse(Console.ReadLine()!);
+
+    volume = Math.PI * (radius * radius) * height;
+
+    if (biggestKegVolume < volume)
+    {
+        biggestKegModel = model;
+        biggestKegVolume = volume;
+    }
+}
+Console.WriteLine(biggestKegModel);
