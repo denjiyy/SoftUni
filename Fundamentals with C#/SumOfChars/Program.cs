b26b366a7f1d22f0a364e39a8dd5279706169a8b
Denis <125385668+denjiyy@users.noreply.github.com>
2024-04-15 16:24:42 +0300

    Add files via upload

---
+++ Fundamentals with C#/SumOfChars/Program.cs
@@ -0,0 +1,11 @@
+﻿int n = int.Parse(Console.ReadLine()!);
+int totalSum = 0;
+
+for (int i = 0; i < n; i++)
+{
+    char letter = char.Parse(Console.ReadLine()!);
+    int asciiCode = (int)letter;
+    totalSum += asciiCode;
+}
+
+Console.WriteLine($"The sum equals: {totalSum}");
