f8c22d44e8ccade3db0a7f4cac393a5d392cf5ef
Denis <125385668+denjiyy@users.noreply.github.com>
2024-04-15 16:35:08 +0300

    Add files via upload

---
+++ Fundamentals with C#/PokeMon/Program.cs
@@ -0,0 +1,22 @@
+﻿int n = int.Parse(Console.ReadLine()!);
+int m = int.Parse(Console.ReadLine()!);
+int y = int.Parse(Console.ReadLine()!);
+
+int originalN = n;
+int pokeCount = 0;
+
+while (n >= m)
+{
+    n -= m;
+    pokeCount++;
+
+    double percent = originalN * 0.5d;
+
+    if (percent == n && y != 0)
+    {
+        n /= y;
+    }
+}
+
+Console.WriteLine(n);
+Console.WriteLine(pokeCount);
