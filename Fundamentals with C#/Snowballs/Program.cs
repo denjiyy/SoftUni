dad1516187529d6db5a796f02ed79f37cca2e496
Denis <125385668+denjiyy@users.noreply.github.com>
2024-04-15 16:36:36 +0300

    Add files via upload

---
+++ Fundamentals with C#/Snowballs/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Numerics;
+
+int n = int.Parse(Console.ReadLine()!);
+BigInteger snowballValue = 0;
+int bestSnow = 0;
+int bestTime = 0;
+int bestQuality = 0;
+
+
+for (int i = 0; i < n; i++)
+{
+    int snowballSnow = int.Parse(Console.ReadLine()!);
+    int snowballTime = int.Parse(Console.ReadLine()!);
+    int snowballQuality = int.Parse(Console.ReadLine()!);
+
+    BigInteger value = BigInteger.Pow((snowballSnow / snowballTime), snowballQuality);
+
+    if (snowballValue < value)
+    {
+        snowballValue = value;
+        bestQuality = snowballQuality;
+        bestSnow = snowballSnow;
+        bestTime = snowballTime;
+    }
+}
+
+Console.WriteLine($"{bestSnow} : {bestTime} = {snowballValue} ({bestQuality})");
