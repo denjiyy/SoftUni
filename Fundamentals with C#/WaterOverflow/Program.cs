481b45966e95b9c50ce6fd9db16117c8596f566e
Denis <125385668+denjiyy@users.noreply.github.com>
2024-04-15 16:30:35 +0300

    Add files via upload

---
+++ Fundamentals with C#/WaterOverflow/Program.cs
@@ -0,0 +1,19 @@
+﻿int n = int.Parse(Console.ReadLine()!);
+int sum = 0;
+int litres;
+int tankCapacity = 255;
+for (int i = 0; i < n; i++)
+{
+    litres = int.Parse(Console.ReadLine()!);
+
+    if (litres > tankCapacity)
+    {
+        Console.WriteLine("Insufficient capacity!");
+    }
+    else
+    {
+        sum += litres;
+        tankCapacity -= litres;
+    }
+}
+Console.WriteLine(sum);
