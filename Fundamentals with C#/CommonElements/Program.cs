aa6d73098c09a286fcf499968c9a5a83d10fdb1a
Denis <125385668+denjiyy@users.noreply.github.com>
2024-04-16 17:35:21 +0300

    Add files via upload

---
+++ Fundamentals with C#/CommonElements/Program.cs
@@ -0,0 +1,23 @@
+﻿List<string> firstArray = Console.ReadLine()!
+    .Split(' ')
+    .ToList();
+
+List<string> secondArray = Console.ReadLine()!
+    .Split(' ')
+    .ToList();
+
+List<string> thirdArray = new List<string>();
+
+for (int i = 0; i < firstArray.Count; i++)
+{
+    for (int j = 0; j < secondArray.Count; j++)
+    {
+        if (firstArray[i] == secondArray[j])
+        {
+            thirdArray.Add(firstArray[i]);
+            break;
+        }
+    }
+}
+
+Console.WriteLine(string.Join(" ", thirdArray));
