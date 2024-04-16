e7e0e5e8c503363daeba79266df542bc3079404a
Denis <125385668+denjiyy@users.noreply.github.com>
2024-04-16 17:41:55 +0300

    Add files via upload

---
+++ Fundamentals with C#/ZigZagArrays/Program.cs
@@ -0,0 +1,32 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+int[] array1 = new int[n];
+int[] array2 = new int[n];
+
+
+for (int i = 0; i < n; i++)
+{
+    string[] pair = Console.ReadLine().Split();
+    array1[i] = int.Parse(pair[0]);
+    array2[i] = int.Parse(pair[1]);
+}
+
+int[] zigzagArray1 = new int[n];
+int[] zigzagArray2 = new int[n];
+
+for (int i = 0; i < n; i++)
+{
+    if (i % 2 == 0)
+    {
+        zigzagArray1[i] = array1[i];
+        zigzagArray2[i] = array2[i];
+    }
+    else
+    {
+        zigzagArray1[i] = array2[i];
+        zigzagArray2[i] = array1[i];
+    }
+}
+
+Console.WriteLine(string.Join(" ", zigzagArray1));
+Console.WriteLine(string.Join(" ", zigzagArray2));
