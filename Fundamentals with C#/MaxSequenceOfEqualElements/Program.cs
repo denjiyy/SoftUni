ffd1a3547bec2b090ae0ded8e23f01d71abafe83
Denis <125385668+denjiyy@users.noreply.github.com>
2024-04-16 18:08:53 +0300

    Add files via upload

---
+++ Fundamentals with C#/MaxSequenceOfEqualElements/Program.cs
@@ -0,0 +1,33 @@
+﻿List<string> symbols = Console.ReadLine()!
+    .Split(' ')
+    .ToList();
+
+int bestCount = 0;
+string bestSymbol = null!;
+
+for (int i = 0; i < symbols.Count; i++)
+{
+    int count = 1;
+    for (int j = i + 1; j < symbols.Count; j++)
+    {
+        if (symbols[i] == symbols[j])
+        {
+            count++;
+
+            if (bestCount < count)
+            {
+                bestCount = count;
+                bestSymbol = symbols[i];
+            }
+        }
+        else
+        {
+            break;
+        }
+    }
+}
+
+for (int i = 0; i < bestCount; i++)
+{
+    Console.Write($"{bestSymbol} ");
+}
