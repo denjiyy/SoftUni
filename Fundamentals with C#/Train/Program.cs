5fa4dc627cccbf118015bd1a883debf49912f8ce
Denis <125385668+denjiyy@users.noreply.github.com>
2024-04-16 17:22:59 +0300

    Add files via upload

---
+++ Fundamentals with C#/Train/Program.cs
@@ -0,0 +1,11 @@
+﻿int numberOfWagons = int.Parse(Console.ReadLine()!);
+
+List<int> peopleInWagons = new List<int>();
+
+for (int i = 0; i < numberOfWagons; i++)
+{
+    peopleInWagons.Add(int.Parse(Console.ReadLine()!));
+}
+
+Console.WriteLine($"{string.Join(" ", peopleInWagons)}");
+Console.WriteLine(peopleInWagons.Sum());
