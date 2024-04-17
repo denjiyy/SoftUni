c61f7a59ddbfbe874b0751e3bb119647dc53ab8e
Denis <125385668+denjiyy@users.noreply.github.com>
2024-04-17 16:31:13 +0300

    Add files via upload

---
+++ Fundamentals with C#/MiddleCharacters/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    private static void MidChars(string input)
+    {
+        int length = input.Length;
+
+        int middle = length / 2;
+
+        if (length % 2 != 0)
+        {
+            Console.WriteLine(input[middle]);
+        }
+        else
+        {
+            int beforeMid = length / 2 - 1;
+            Console.Write(input[beforeMid]);
+            Console.Write(input[middle]);
+        }
+    }
+
+    static void Main()
+    {
+        string input = Console.ReadLine()!;
+        MidChars(input);
+        Console.ReadLine();
+    }
+}
