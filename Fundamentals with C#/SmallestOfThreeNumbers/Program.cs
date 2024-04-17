ed7a0c18a5ff518d0cbaef9f67336ce9cdb9b1fa
Denis <125385668+denjiyy@users.noreply.github.com>
2024-04-17 15:55:03 +0300

    Add files via upload

---
+++ Fundamentals with C#/SmallestOfThreeNumbers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static int MinNumber(List<int> list)
+    {
+        int count = list.Count;
+        int smallestNumber = list[0];
+
+        for (int i = 0; i < count; i++)
+        {
+            if (list[i] < smallestNumber)
+            {
+                smallestNumber = list[i];
+            }
+        }
+
+        return smallestNumber;
+    }
+    static void Main(string[] args)
+    {
+        List<int> list = new List<int>();
+
+        for (int i = 0; i < 3; i++)
+        {
+            list.Add(int.Parse(Console.ReadLine()!));
+        }
+
+        Console.WriteLine(MinNumber(list));
+    }
+}
