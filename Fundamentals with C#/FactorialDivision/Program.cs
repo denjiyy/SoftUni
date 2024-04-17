4d2d6a579d842e8ecee80b7322e6278ed43f678f
Denis <125385668+denjiyy@users.noreply.github.com>
2024-04-17 16:36:40 +0300

    Add files via upload

---
+++ Fundamentals with C#/FactorialDivision/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+
+class Program
+{
+    private static double Recursive(double x)
+    {
+        if (x == 1) return 1;
+        else return x * Recursive(x - 1);
+    }
+
+    private static double SumDiv(double x, double y)
+    {
+        return x / y;
+    }
+
+    static void Main()
+    {
+        double x = double.Parse(Console.ReadLine()!);
+        double y = double.Parse(Console.ReadLine()!);
+
+        Console.WriteLine($"{SumDiv(Recursive(x), Recursive(y)):f2}");
+    }
+}
