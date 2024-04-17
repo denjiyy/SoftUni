652349ed3876eb4acdc59cf97e4cabad8f7763c8
Denis <125385668+denjiyy@users.noreply.github.com>
2024-04-17 15:59:34 +0300

    Add files via upload

---
+++ Fundamentals with C#/VowelsCount/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Reflection;
+
+class Program
+{
+    static int CountVowels(string str)
+    {
+        int count = 0;
+
+        for (int i = 0; i < str.Length; i++)
+        {
+            if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u')
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    static void Main(string[] args)
+    {
+        string str = Console.ReadLine()!
+            .ToLower();
+
+        Console.WriteLine(CountVowels(str));
+    }
+}
