cc5e28febdea70593e6d972aeb8d7e96249e4a05
Denis <125385668+denjiyy@users.noreply.github.com>
2024-04-17 16:33:18 +0300

    Add files via upload

---
+++ Fundamentals with C#/NxNMatrix/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+
+class Program
+{
+    private static void Matrix(int matrix)
+    {
+        for (int i = 0; i < matrix; i++)
+        {
+            int[] row = new int[matrix];
+            for (int j = 0; j < matrix; j++)
+            {
+                row[j] = matrix;
+            }
+            Console.WriteLine(string.Join(" ", row));
+        }
+    }
+
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine()!);
+        Matrix(n);
+        Console.ReadLine();
+    }
+}
