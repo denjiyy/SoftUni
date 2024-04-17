afcbe967f5bd7ac6a8259772485a0db388750f3d
Denis <125385668+denjiyy@users.noreply.github.com>
2024-04-17 16:05:02 +0300

    Add files via upload

---
+++ Fundamentals with C#/CharactersInRange/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void PrintCharactersBetween(char char1, char char2)
+    {
+        if (char2 < char1)
+        {
+            char temp = char1;
+            char1 = char2;
+            char2 = temp;
+        }
+
+        for (char c = (char)(char1 + 1); c < char2; c++)
+        {
+            Console.Write(c + " ");
+        }
+
+        Console.WriteLine();
+    }
+
+    static void Main(string[] args)
+    {
+        char char1 = char.Parse(Console.ReadLine()!);
+        char char2 = char.Parse(Console.ReadLine()!);
+
+        PrintCharactersBetween(char1, char2);
+    }
+}
