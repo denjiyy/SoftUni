b8d9643b4f90e00ea63ba7a459fe7404dcc72fde
Denis <125385668+denjiyy@users.noreply.github.com>
2024-04-17 16:21:46 +0300

    Add files via upload

---
+++ Fundamentals with C#/AddAndSubtract/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static int SumFirstTwo(int a, int b)
+    {
+        return a + b;
+    }
+
+    static int SubtractThird(int sum, int c)
+    {
+        return sum - c;
+    }
+
+    static void Main(string[] args)
+    {
+        int a = int.Parse(Console.ReadLine()!);
+        int b = int.Parse(Console.ReadLine()!);
+        int c = int.Parse(Console.ReadLine()!);
+
+        Console.WriteLine(SubtractThird(SumFirstTwo(a, b), c));
+    }
+}
