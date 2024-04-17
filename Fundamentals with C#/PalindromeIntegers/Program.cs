46cdbaa14dc9e50729701ffac116792d78bb41b0
Denis <125385668+denjiyy@users.noreply.github.com>
2024-04-17 16:40:04 +0300

    Add files via upload

---
+++ Fundamentals with C#/PalindromeIntegers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static bool IsPalindrome(int number)
+    {
+        string numberStr = number.ToString();
+        char[] reversedChars = numberStr.ToCharArray();
+        Array.Reverse(reversedChars);
+        string reversedStr = new string(reversedChars);
+        return numberStr == reversedStr;
+    }
+
+    static void CheckPalindrome()
+    {
+        while (true)
+        {
+            string input = Console.ReadLine()!;
+            if (input == "END")
+                break;
+
+            int number = int.Parse(input);
+            bool isPalindrome = IsPalindrome(number);
+            Console.WriteLine(isPalindrome.ToString().ToLower());
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        CheckPalindrome();
+    }
+}
