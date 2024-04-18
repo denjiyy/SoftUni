8821e44acc8f6207ee22b0323d3d69fc56d40b3f
Denis <125385668+denjiyy@users.noreply.github.com>
2024-04-18 17:45:43 +0300

    Add files via upload

---
+++ Fundamentals with C#/ListOperations/Program.cs
@@ -0,0 +1,77 @@
+﻿List<int> list = Console.ReadLine()!
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+while (true)
+{
+    string command = Console.ReadLine()!;
+
+    if (command == "End")
+    {
+        break;
+    }
+
+    string[] elements = command.Split();
+
+    if (elements[0] == "Add")
+    {
+        int number = int.Parse(elements[1]);
+        list.Add(number);
+    }
+
+    if (elements[0] == "Insert")
+    {
+        int number = int.Parse(elements[1]);
+        int index = int.Parse(elements[2]);
+
+        if (index >= 0 && index < list.Count)
+        {
+            list.Insert(index, number);
+        }
+        else
+        {
+            Console.WriteLine("Invalid index");
+        }
+    }
+
+    if (elements[0] == "Remove")
+    {
+        int index = int.Parse(elements[1]);
+
+        if (index >= 0 && index < list.Count)
+        {
+            list.RemoveAt(index);
+        }
+        else
+        {
+            Console.WriteLine("Invalid index");
+        }
+    }
+
+    if (elements[0] == "Shift" && elements[1] == "left")
+    {
+        int count = int.Parse(elements[2]);
+
+        for (int i = 0; i < count % list.Count; i++)
+        {
+            int firstEl = list[0];
+            list.RemoveAt(0);
+            list.Add(firstEl);
+        }
+    }
+
+    if (elements[0] == "Shift" && elements[1] == "right")
+    {
+        int count = int.Parse(elements[2]);
+
+        for (int i = 0; i < count % list.Count; i++)
+        {
+            int lastEl = list[list.Count - 1];
+            list.RemoveAt(list.Count - 1);
+            list.Insert(0, lastEl);
+        }
+    }
+}
+
+Console.WriteLine(string.Join(" ", list));
