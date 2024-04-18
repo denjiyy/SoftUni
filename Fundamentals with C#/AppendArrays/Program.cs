e9de1de1fe0482749eab2479c72fdea975f94821
Denis <125385668+denjiyy@users.noreply.github.com>
2024-04-18 17:57:05 +0300

    Add files via upload

---
+++ Fundamentals with C#/AppendArrays/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        string[] stringsArrays = Console.ReadLine()!
+            .Split(new[] { '|' }, StringSplitOptions.RemoveEmptyEntries);
+
+        List<string> symbols = ExtractSymbols(stringsArrays);
+
+        Console.WriteLine(string.Join(" ", symbols));
+    }
+
+    private static List<string> ExtractSymbols(string[] stringsArrays)
+    {
+        List<string> result = new List<string>();
+
+        for (int i = stringsArrays.Length - 1; i >= 0; i--)
+        {
+            string[] array = stringsArrays[i]
+                .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            result.AddRange(array);
+        }
+
+        return result;
+    }
+}
