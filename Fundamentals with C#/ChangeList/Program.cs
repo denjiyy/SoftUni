1de2e55280e1727d283096840068be91edfdd857
Denis <125385668+denjiyy@users.noreply.github.com>
2024-04-18 17:39:48 +0300

    Add files via upload

---
+++ Fundamentals with C#/ChangeList/Program.cs
@@ -0,0 +1,29 @@
+﻿List<int> integers = Console.ReadLine()!
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToList();
+
+string command;
+while ((command = Console.ReadLine()!) != "end")
+{
+    List<string> commandTokens = command
+        .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+        .ToList();
+
+    if (commandTokens[0] == "Delete")
+    {
+        for (int i = 0; i < integers.Count; i++)
+        {
+            if (int.Parse(commandTokens[1]) == integers[i])
+            {
+                integers.Remove(integers[i]);
+            }
+        }
+    }
+    else if (commandTokens[0] == "Insert")
+    {
+        integers.Insert(int.Parse(commandTokens[2]), int.Parse(commandTokens[1]));
+    }
+}
+
+Console.WriteLine(string.Join(" ", integers));
