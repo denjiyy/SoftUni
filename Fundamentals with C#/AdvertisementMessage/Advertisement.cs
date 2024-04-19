4f5d03c2a47210c87bf2bb81ded8177d88a88e8d
Denis <125385668+denjiyy@users.noreply.github.com>
2024-04-19 19:29:41 +0300

    Add files via upload

---
+++ Fundamentals with C#/AdvertisementMessage/Advertisement.cs
@@ -0,0 +1,61 @@
+﻿public class Advertisement
+{
+    public string[] phrases = new string[] {
+            "Excellent product.",
+            "Such a great product.",
+            "I always use that product.",
+            "Best product of its category.",
+            "Exceptional product.",
+            "I can't live without this product."
+        };
+
+    public string[] events = new string[]
+    {
+            "Now I feel good.",
+            "I have succeeded with this product.",
+            "Makes miracles. I am happy of the results!",
+            "I cannot believe but now I feel awesome.",
+            "Try it yourself, I am very satisfied.",
+            "I feel great!"
+    };
+
+    public string[] authors = new string[]
+    {
+            "Diana",
+            "Petya",
+            "Stella",
+            "Elena",
+            "Katya",
+            "Iva",
+            "Annie",
+            "Eva"
+    };
+
+    public string[] cities = new string[]
+    {
+            "Burgas",
+            "Sofia",
+            "Plovdiv",
+            "Varna",
+            "Ruse"
+    };
+
+    public string GenerateRandomMessage()
+    {
+        Random rnd = new Random();
+
+        int index = rnd.Next(phrases.Length);
+        string phrase = phrases[index];
+
+        index = rnd.Next(events.Length);
+        string ev = events[index];
+
+        index = rnd.Next(authors.Length);
+        string auth = authors[index];
+
+        index = rnd.Next(cities.Length);
+        string city = cities[index];
+
+        return $"{phrase} {ev} {auth} - {city}.";
+    }
+}
---
+++ Fundamentals with C#/AdvertisementMessage/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Objects_and_Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine()!);
+
+            for (int i = 0; i < n; i++)
+            {
+                Advertisement ad = new Advertisement();
+                Console.WriteLine(ad.GenerateRandomMessage());
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
