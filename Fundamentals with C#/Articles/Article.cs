d480f3d76037740b20ff1d4923cf3ceaccd9b202
Denis <125385668+denjiyy@users.noreply.github.com>
2024-04-19 20:04:09 +0300

    Add files via upload

---
+++ Fundamentals with C#/Articles/Article.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Articles
+{
+    public class Article
+    {
+        public Article(string title, string content, string author)
+        {
+            Title = title;
+            Content = content;
+            Author = author;
+        }
+
+        public string Title { get; private set; }
+        public string Content { get; private set; }
+        public string Author { get; private set; }
+
+        public void Edit(string newContent)
+        {
+            Content = newContent;
+        }
+
+        public void ChangeAuthor(string newAuthor)
+        {
+            Author = newAuthor;
+        }
+
+        public void Rename(string newTitle)
+        {
+            Title = newTitle;
+        }
+
+        public override string ToString()
+        {
+            return $"{Title} - {Content}: {Author}";
+        }
+    }
+}
---
+++ Fundamentals with C#/Articles/Program.cs
@@ -0,0 +1,42 @@
+﻿using Articles;
+using System;
+
+class Program
+{
+    private static void Main(string[] args)
+    {
+        List<string> book = Console.ReadLine()!
+            .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+            .ToList();
+
+        Article article = new Article(book[0], book[1], book[2]);
+
+        int n = int.Parse(Console.ReadLine()!);
+
+        while (n > 0)
+        {
+            List<string> commands = Console.ReadLine()!
+                .Split(":", StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            switch (commands[0])
+            {
+                case "Edit":
+                    article.Edit(commands[1]);
+                    break;
+                case "ChangeAuthor":
+                    article.ChangeAuthor(commands[1]);
+                    break;
+                case "Rename":
+                    article.Rename(commands[1]);
+                    break;
+                default:
+                    break;
+            }
+
+            n--;
+        }
+
+        Console.WriteLine(article.ToString());
+    }
+}
