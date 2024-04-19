200d502cba31d438f1367c48e9452ffa6dc77bbf
Denis <125385668+denjiyy@users.noreply.github.com>
2024-04-19 20:23:41 +0300

    Add files via upload

---
+++ Fundamentals with C#/Articles2.0/Article.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Articles2._0
+{
+    public class Article
+    {
+        public Article(string title, string content, string author)
+        {
+            Title = title;
+            Content = content;
+            Author = author;
+            Articles = new List<string>();
+        }
+
+        public string Title { get; private set; }
+        public string Content { get; private set; }
+        public string Author { get; private set; }
+
+        public List<string> Articles;
+
+        public void Edit(string newContent)
+        {
+            Content = newContent;
+        }
+
+        public void ChangeAuthor(string newAuthor)
+        {
+            Author = newAuthor;
+        }
+
+        public void Rename(string newTitle)
+        {
+            Content = newTitle;
+        }
+
+        public override string ToString()
+        {
+            return $"{Title} - {Content}: {Author}";
+        }
+    }
+}
---
+++ Fundamentals with C#/Articles2.0/Program.cs
@@ -0,0 +1,24 @@
+﻿using Articles2._0;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine()!);
+        List<Article> articles = new List<Article>();
+
+        while (n > 0)
+        {
+            List<string> articleTokens = Console.ReadLine()!
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            articles.Add(new Article(articleTokens[0], articleTokens[1], articleTokens[2]));
+
+            n--;
+        }
+
+        Console.WriteLine(string.Join("\n", articles));
+    }
+}
