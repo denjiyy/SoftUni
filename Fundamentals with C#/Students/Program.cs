8979515e1ae60540cc9e639db59356b084ca9c9b
Denis <125385668+denjiyy@users.noreply.github.com>
2024-04-20 12:30:27 +0300

    Add files via upload

---
+++ Fundamentals with C#/Students/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    private static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine()!);
+
+        List<Students> students = new List<Students>();
+        while (n > 0)
+        {
+            List<string> studentsTokens = Console.ReadLine()!
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            students.Add(new Students(studentsTokens[0], studentsTokens[1], float.Parse(studentsTokens[2])));
+
+            n--;
+        }
+
+        Console.WriteLine(string.Join("\n", students));
+    }
+}
---
+++ Fundamentals with C#/Students/Students.cs
@@ -0,0 +1,18 @@
+﻿public class Students
+{
+    public Students(string firstName, string lastName, float grade)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Grade = grade;
+    }
+
+    public string FirstName { get; private set; }
+    public string LastName { get; private set; }
+    public float Grade { get; private set; }
+
+    public override string ToString()
+    {
+        return $"{FirstName} {LastName}: {Grade:f2}";
+    }
+}
