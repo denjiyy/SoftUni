ff30375a79df6047c0feabaa21fdcf8beb3b79bf
Denis <125385668+denjiyy@users.noreply.github.com>
2024-04-22 19:51:05 +0300

    Add files via upload

---
+++ Fundamentals with C#/VehicleCatalogue/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace VehicleCatalogue
+{
+    internal class Program
+    {
+        internal static void Main(string[] args)
+        {
+            HashSet<Vehicle> vehicleCatalogue = new HashSet<Vehicle>();
+
+            string command;
+
+            while ((command = Console.ReadLine()!) != "End")
+            {
+                List<string> commandsTokens = command
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .ToList();
+
+                vehicleCatalogue.Add(new Vehicle(commandsTokens[0], commandsTokens[1], commandsTokens[2], int.Parse(commandsTokens[3])));
+            }
+
+            string token;
+            while ((token = Console.ReadLine()!) != "Close the Catalogue")
+            {
+                var matchedVehicle = vehicleCatalogue.FirstOrDefault(x => x.Model.ToLower() == token.ToLower());
+                if (matchedVehicle != null)
+                {
+                    Console.WriteLine(matchedVehicle.ToString());
+                }
+            }
+
+            Console.WriteLine(string.Join("\n", vehicleCatalogue));
+            Console.WriteLine($"Cars have average horsepower of: {vehicleCatalogue.Where(x => x.Type.ToLower() == "car").Average(x => x.HorsePower)}");
+            Console.WriteLine($"Trucks have average horsepower of: {vehicleCatalogue.Where(x => x.Type.ToLower() == "truck").Average(x => x.HorsePower)}");
+        }
+    }
+}
---
+++ Fundamentals with C#/VehicleCatalogue/Vehicle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehicleCatalogue
+{
+    public class Vehicle
+    {
+        public Vehicle(string type, string model, string color, int horsePower)
+        {
+            Type = type;
+            Model = model;
+            Color = color;
+            HorsePower = horsePower;
+        }
+
+        public string Type { get; private set; }
+        public string Model { get; private set; }
+        public string Color { get; private set; }
+        public int HorsePower { get; private set; }
+
+        public override string ToString()
+        {
+            return $"Type: {Type}\nModel: {Model}\nColor: {Color}\nHorsepower: {HorsePower}";
+        }
+    }
+}
