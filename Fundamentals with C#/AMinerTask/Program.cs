b54e1f5c3bad7e4c15f8ca5dbe2bec480b162c75
Denis <125385668+denjiyy@users.noreply.github.com>
2024-04-25 17:02:15 +0300

    Add files via upload

---
+++ Fundamentals with C#/AMinerTask/Program.cs
@@ -0,0 +1,19 @@
+﻿var dictionary = new Dictionary<string, int>();
+
+string input;
+int quantity;
+while ((input = Console.ReadLine()!) != "stop")
+{
+    if (!dictionary.ContainsKey(input))
+    {
+        dictionary.Add(input, 0);
+    }
+
+    quantity = int.Parse(Console.ReadLine()!);
+    dictionary[input] += quantity;
+}
+
+foreach (var item in dictionary)
+{
+    Console.WriteLine(item.Key + " -> " + item.Value);
+}
