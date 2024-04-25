85df50b295151140472c826bc5ca2a0754c29891
Denis <125385668+denjiyy@users.noreply.github.com>
2024-04-25 16:56:45 +0300

    Add files via upload

---
+++ Fundamentals with C#/CountCharsInAString/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var dictionary = new Dictionary<char, int>();
+
+        foreach (var item in Console.ReadLine()!)
+        {
+            int count = 0;
+
+            if (item == ' ')
+            {
+                continue;
+            }
+
+            if (dictionary.ContainsKey(item))
+            {
+                count = dictionary[item];
+            }
+
+            dictionary[item] = count + 1;
+        }
+
+        foreach (var item in dictionary)
+        {
+            Console.WriteLine(item.Key + " -> " + item.Value);
+        }
+    }
+}
