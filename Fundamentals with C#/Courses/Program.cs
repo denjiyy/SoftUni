02d4e0acad72fd6d881cdd5e3b1c21eebc3fde67
Denis <125385668+denjiyy@users.noreply.github.com>
2024-04-25 17:19:33 +0300

    Add files via upload

---
+++ Fundamentals with C#/Courses/Program.cs
@@ -0,0 +1,31 @@
+﻿Dictionary<string, List<string>> course = new Dictionary<string, List<string>>();
+
+string input;
+while ((input = Console.ReadLine()!) != "end")
+{
+    List<string> commands = input
+        .Split(" : ", StringSplitOptions.RemoveEmptyEntries)
+        .ToList();
+
+    string courseName = commands[0];
+    string studentName = commands[1];
+
+    if (course.ContainsKey(courseName))
+    {
+        course[courseName].Add(studentName);
+    }
+    else
+    {
+        course.Add(courseName, new List<string> { studentName });
+    }
+}
+
+foreach (var item in course)
+{
+    Console.WriteLine($"{item.Key}: {item.Value.Count}");
+
+    foreach (var student in item.Value)
+    {
+        Console.WriteLine($"-- {student}");
+    }
+}
