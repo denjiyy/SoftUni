2bb19555d04039d4dc3eb05985326a84d3ccbe5d
Denis <125385668+denjiyy@users.noreply.github.com>
2024-04-25 17:28:28 +0300

    Add files via upload

---
+++ Fundamentals with C#/StudentAcademy/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace StudentAcademy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Students> dictionary = new Dictionary<string, Students>();
+
+            int n = int.Parse(Console.ReadLine()!);
+
+            string student;
+            double grade;
+
+            for (int i = 0; i < n; i++)
+            {
+                student = Console.ReadLine()!;
+                grade = double.Parse(Console.ReadLine()!);
+
+                if (!dictionary.ContainsKey(student))
+                {
+                    dictionary.Add(student, new Students(student));
+                }
+
+                dictionary[student].Grades.Add(grade);
+            }
+
+            Dictionary<string, Students> filteredStudents = dictionary
+                .Where(x => x.Value.Grades.Average() >= 4.5)
+                .ToDictionary(x => x.Key, x => x.Value);
+
+            foreach (var item in filteredStudents)
+            {
+                Console.WriteLine(item.Value);
+            }
+        }
+    }
+}
---
+++ Fundamentals with C#/StudentAcademy/Students.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentAcademy
+{
+    public class Students
+    {
+        public Students(string name)
+        {
+            Name = name;
+            Grades = new List<double>();
+        }
+
+        public string Name { get; private set; }
+        public List<double> Grades { get; private set; }
+
+        public override string ToString()
+        {
+            double averageGrade = Grades.Count > 0 ? Grades.Average() : 0.0;
+            return $"{Name} -> {averageGrade:f2}";
+        }
+    }
+}
