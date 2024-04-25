96de58be279699953e292e2b8ace769abcd9be4a
Denis <125385668+denjiyy@users.noreply.github.com>
2024-04-25 17:10:19 +0300

    Add files via upload

---
+++ Fundamentals with C#/Orders/Product.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orders
+{
+    public class Product
+    {
+        public Product(string name, double price, int quantity)
+        {
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+        }
+
+        public string Name { get; private set; }
+        public double Price { get; private set; }
+        public int Quantity { get; private set; }
+
+        public void Update(double price, int quantity)
+        {
+            Price = price;
+            Quantity = quantity;
+        }
+
+        public double TotalPrice()
+        {
+            return Price + Quantity;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} -> {TotalPrice():F2}";
+        }
+    }
+}
---
+++ Fundamentals with C#/Orders/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Orders
+{
+    internal static class Program
+    {
+        static void Main(string[] args)
+        {
+            var dictionary = new Dictionary<string, Product>();
+
+            string input;
+            while ((input = Console.ReadLine()!) != "buy")
+            {
+                List<string> commands = input
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .ToList();
+
+                Product product = new Product(commands[0], double.Parse(commands[1]), int.Parse(commands[2]));
+
+                if (!dictionary.ContainsKey(commands[0]))
+                {
+                    dictionary.Add(commands[0], product);
+                }
+                else
+                {
+                    dictionary[commands[0]].Update(double.Parse(commands[1]), int.Parse(commands[2]));
+                }
+            }
+
+            foreach (var item in dictionary)
+            {
+                Console.WriteLine(item.Value);
+            }
+        }
+    }
+}
