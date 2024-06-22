9d2e2c548a51c72f2c032acd2a785bb9562da773
Denis <125385668+denjiyy@users.noreply.github.com>
2024-06-22 19:40:35 +0300

    Add files via upload

---
+++ C# DB/C# DB Second Half/EmployeesFullInformation/DatabaseFirst/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using SoftUni.Data;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            SoftUniContext context = new SoftUniContext();
+            Console.WriteLine(GetEmployeesFullInformation(context));
+        }
+
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            return string.Join(Environment.NewLine, context.Employees.
+                Select(e => $"{e.FirstName} {e.LastName} {e.MiddleName} {e.JobTitle} {e.Salary:f2}")
+                .ToList());
+        }
+    }
+}
